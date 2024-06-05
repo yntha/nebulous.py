d34fadc458cda90afc55c86cb519fd1fb6c27f91
yntha <126660548+yntha@users.noreply.github.com>
2024-06-05 14:07:29 -0400

    Add models representing serialized game objects
    These models are deserialized upon reading the Game Data packet

---
+++ src/nebulous/game/models/netobjects.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+
+from nebulous.game.natives import CompressedFloat, MUTF8String, VariableLengthArray
+
+
+@dataclass
+class NetPlayer:
+    player_id: int  # 1 byte
+    skin_id: int  # 2 bytes
+    eject_skin_id: int  # 1 byte
+    custom_skin_id: int  # 4 bytes
+    custom_pet_id: int  # 4 bytes
+    pet_id: int  # 1 byte
+    pet_level: int  # 2 bytes
+    pet_name: MUTF8String
+    hat_id: int  # 1 byte
+    halo_id: int  # 1 byte
+    pet_id2: int  # 1 byte
+    pet_level2: int  # 2 bytes
+    pet_name2: MUTF8String
+    custom_pet_id2: int  # 4 bytes
+    custom_particle_id: int  # 4 bytes
+    particle_id: int  # 1 byte
+    level_colors: VariableLengthArray  # length size 1
+    name_animation_id: int  # 1 byte
+    skin_id2: int  # 2 bytes
+    skin_interpolation_rate: CompressedFloat  # 2 bytes
+    custom_skin_id2: int  # 4 bytes
+    blob_color: int  # 4 bytes
+    team_id: int  # 1 byte
+    player_name: MUTF8String
+    font_id: int  # 1 byte
+    alias_colors: VariableLengthArray
+    account_id: int  # 4 bytes
+    player_level: int  # 2 bytes
+    clan_name: MUTF8String
+    clan_colors: VariableLengthArray
+    clan_role: int  # 1 byte
+    click_type: int  # 1 byte
+
+
+@dataclass
+class NetPlayerEject:
+    eject_id: int  # 1 byte
+    xpos: CompressedFloat  # 3 bytes, relative to GameData.map_size
+    ypos: CompressedFloat  # 3 bytes, relative to GameData.map_size
+    mass: CompressedFloat  # 3 bytes, relative to 500000.0
+
+
+@dataclass
+class NetGameDot:
+    dot_id: int
+    xpos: CompressedFloat  # 3 bytes, relative to GameData.map_size
+    ypos: CompressedFloat  # 3 bytes, relative to GameData.map_size
+
+
+@dataclass
+class NetGameItem:
+    item_id: int
+    item_type: int  # 1 byte
+    xpos: CompressedFloat  # 3 bytes, relative to GameData.map_size
+    ypos: CompressedFloat  # 3 bytes, relative to GameData.map_size
