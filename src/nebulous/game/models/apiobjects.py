08ea21210fd085e879e9fe10415431d6360e993d
yntha <126660548+yntha@users.noreply.github.com>
2024-06-06 16:22:19 -0400

    Add new api models
    Added two new api model structures, `APIPlayerStats`, and
    `APIPlayerGeneralStats`.

--- src/nebulous/game/models/apiobjects.py
+++ src/nebulous/game/models/apiobjects.py
@@ -42,6 +42,97 @@ class APIPlayerProfile:
     bio_fonts: list[int] = field(default_factory=([0x00] * 23).copy)
 
 
+@dataclass
+class APIPlayerGeneralStats:
+    xp: int
+    dots_eaten: int
+    blobs_eaten: int
+    blobs_lost: int
+    biggest_blob: int
+    mass_gained: int
+    mass_ejected: int
+    eject_count: int
+    split_count: int
+    average_score: int
+    highest_score: int
+    times_restarted: int
+    longest_life_ms: int
+    games_won: int
+    smbh_collided_count: int
+    smbh_eaten_count: int
+    bh_collided_count: int
+    arenas_won: int
+    clan_wars_won: int
+    tbh_collided_count: int
+    times_teleported: int
+    powerups_used: int
+    trick_count: int
+    matches_won: int
+    challenges_won: int
+    years_played: int
+    accolades: int
+    max_plasma_chain: int
+    coins_collected: int
+    triangles_destroyed: int
+    squares_destroyed: int
+    pentagons_destroyed: int
+    hexagons_destroyed: int
+    players_killed: int
+    shots_fired: int
+    damage_dealt: int
+    damage_taken: int
+    damage_healed: int
+    achievements_earned: list[int] = field(default_factory=[].copy)
+    achievement_stats: list[Any] = field(default_factory=[].copy)  # i havent been able to figure this out yet
+    special_objects: list[dict[str, str]] = field(default_factory=[].copy)
+
+
+@dataclass
+class APIPlayerStats:
+    account_id: int
+    account_name: str
+    competition_banned: bool
+    competition_banned_until_ms: int
+    chat_banned: bool
+    chat_banned_until_ms: int
+    is_supporter: bool
+    dq: int
+    dq_done: int
+    xp_multiplier: int
+    xp_multiplier_s: int
+    mass_boost: int
+    mass_boost_s: int
+    plasma_boost: int
+    plasma_boost_s: int
+    click_type: int
+    click_type_s: int
+    length_boost: int
+    length_boost_s: int
+    purchased_alias_colors: bool
+    purchased_clan_colors: bool
+    purchased_blob_color: bool
+    click_enabled: bool
+    xp_boost_enabled: bool
+    mass_boost_enabled: bool
+    current_coins: int
+    purchased_skin_map: bool
+    purchased_second_pet: bool
+    unlocked_multiskin: bool
+    is_apple_guest: bool
+    clan_member: ClanMember
+    clan: Clan
+    general_stats: APIPlayerGeneralStats
+    account_colors: list[int] = field(default_factory=[].copy)
+    purchased_avatars: list[int] = field(default_factory=[].copy)
+    purchased_eject_skins: list[int] = field(default_factory=[].copy)
+    purchased_hats: list[int] = field(default_factory=[].copy)
+    purchashed_particles: list[int] = field(default_factory=[].copy)
+    purchased_halos: list[int] = field(default_factory=[].copy)
+    purchased_pets: list[int] = field(default_factory=[].copy)
+    valid_custom_skin_ids: list[int] = field(default_factory=[].copy)
+    valid_custom_pet_ids: list[int] = field(default_factory=[].copy)
+    valid_custom_particle_ids: list[int] = field(default_factory=[].copy)
+    clan_colors: list[int] = field(default_factory=[].copy)
 
 
 @dataclass
