47c1c6e944613b5062de7cee8f486f622af3e2c6
yntha <126660548+yntha@users.noreply.github.com>
2024-06-13 23:22:29 -0400

    Add model `XPSetEvent`
    Represents a triggered event that occurs when a player's XP is set or updated with tempboosts. Click type is not specified here, but plasma boost is, but plasma boost duration is not specified. This protocol is such a mess.

--- src/nebulous/game/models/gameevents.py
+++ src/nebulous/game/models/gameevents.py
@@ -71,3 +71,24 @@ class AchievementEarnedEvent(GameEvent):
         achievement_id (int): The ID of the achievement that was earned.
     """
     achievement_id: int  # 2 bytes
+
+
+@dataclass
+class XPSetEvent(GameEvent):
+    """
+    Represents a triggered event that occurs when a player's XP is set or updated
+    with tempboosts. Click type is not specified here, but plasma boost is, but
+    plasma boost duration is not specified. This protocol is such a mess.
+
+    Attributes:
+        player_xp (int): The player's new XP value.
+        xp_mult_type (int): The type of XP boost that was set.
+        xp_duration_s (int): The duration of the XP boost in seconds.
+        plasma_boost_type (int): The type of plasma boost that was set.
+        click_type_duration_s (int): The duration of the click type in seconds.
+    """
+    player_xp: int  # 8 bytes
+    xp_mult_type: int  # 1 byte
+    xp_duration_s: int  # 4 bytes
+    plasma_boost_type: int  # 1 byte
+    click_type_duration_s: int  # 4 bytes
