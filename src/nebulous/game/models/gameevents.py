55945f623dc17b32c8dc9cca5d6c5f5c70d67a50
yntha <126660548+yntha@users.noreply.github.com>
2024-06-13 23:00:07 -0400

    Add model `BlobExplodeEvent`
    Represents an event when a player blob explodes.

--- src/nebulous/game/models/gameevents.py
+++ src/nebulous/game/models/gameevents.py
@@ -12,3 +12,16 @@ class GameEvent:
         event_type (GameEventType): The type of event that was triggered.
     """
     event_type: GameEventType
+
+
+@dataclass
+class BlobExplodeEvent(GameEvent):
+    """
+    Represents an event when a player blob explodes.
+
+    Attributes:
+        player_id (int): The ID of the player that triggered the event.
+        blob_id (int): The ID of the player blob that exploded.
+    """
+    player_id: int
+    blob_id: int
